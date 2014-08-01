3ead34bf19da5ff348ac474734e671c810ee7af5
DrakeMagi <dragondust99@yahoo.com>
2014-08-01 13:16:13 -0400

    screen.handler

--- word_storm.py
+++ word_storm.py
@@ -7,7 +7,7 @@ pygame.init()
 
 if __name__ == '__main__':
     handler = screen.Handler('Word Storm', (1024, 768))
-    handler.scenes['intro'] = intro_scene.Intro(handler)
-    handler.scenes['game'] = game_scene.Game(handler)
+    screen.handler.scenes['intro'] = intro_scene.Intro()
+    screen.handler.scenes['game'] = game_scene.Game()
     handler.loop('intro', 30)
     pygame.quit()
